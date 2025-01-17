88cd02be6f260cf527a5169c918a727738da7eca
Siddhartha Sarker <79743342+iamsiddhartha10@users.noreply.github.com>
2025-01-18 01:46:38 +0530

    Delete prisma directory

--- prisma/migrations/20250116220324_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Appointment" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
-);
